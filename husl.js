6a7b0dc9d529d324dc4251b96819997789465a41
Chadwick Dahlquist <chadwick@bugeats.net>
2015-10-22 12:47:07 -0700

    Remove dep on old husl  function, and use new husl public methods

--- husl.js
+++ husl.js
@@ -1,17 +1,29 @@
 var husl = require('husl');
 var stylus = require('stylus');
 
-module.exports = function() {
-    return function(style) {
+module.exports = function () {
+
+    // convert [0-1] (rgb) values to [0-255] (stylus) values
+    function floatToInt(f) {
+        return Math.round(f * 255);
+    }
+
+    return function (style) {
         style.define('husl', function (H, S, L, A) {
-            var rgb = husl._rgbPrepare(husl._conv.husl.rgb([H.val, S.val, L.val]));
+            var rgb = husl.toRGB(H.val, S.val, L.val);
+            var r = floatToInt(rgb[0]);
+            var g = floatToInt(rgb[1]);
+            var b = floatToInt(rgb[2]);
             var a = (A !== undefined ? A.val : 1);
-            return new stylus.nodes.RGBA(rgb[0], rgb[1], rgb[2], a);
+            return new stylus.nodes.RGBA(r, g, b, a);
         });
         style.define('huslp', function (H, S, L, A) {
-            var rgb = husl._rgbPrepare(husl._conv.huslp.rgb([H.val, S.val, L.val]));
+            var rgb = husl.p.toRGB(H.val, S.val, L.val);
+            var r = floatToInt(rgb[0]);
+            var g = floatToInt(rgb[1]);
+            var b = floatToInt(rgb[2]);
             var a = (A !== undefined ? A.val : 1);
-            return new stylus.nodes.RGBA(rgb[0], rgb[1], rgb[2], a);
+            return new stylus.nodes.RGBA(r, g, b, a);
         });
     }
 }
